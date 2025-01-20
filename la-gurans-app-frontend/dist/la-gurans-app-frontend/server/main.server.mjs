31b4a9a36d3aca711318b03c405a3d92bb1825ba
Bibek Adhikari <bibekadhikari2141@gmail.com>
2025-01-20 10:51:32 -0800

    New Update

--- la-gurans-app-frontend/dist/la-gurans-app-frontend/server/main.server.mjs
+++ la-gurans-app-frontend/dist/la-gurans-app-frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-Y7XRXKJ2.mjs";import"./chunk-UGUBSMTO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-HUC3VAXQ.mjs";import"./chunk-UGUBSMTO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
